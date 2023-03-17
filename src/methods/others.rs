eac0e7228d1a371916dc1bc64c8eeffbd36b5dcc
SAMI BETTAYEB <sami3639@gmail.com>
2023-03-18 00:48:28 +0100

    Add others configuration
    Signed-off-by: SAMI BETTAYEB <sami3639@gmail.com>

---
+++ src/methods/others.rs
@@ -0,0 +1,269 @@
+use std::env;
+
+use lazy_static::lazy_static;
+
+use crate::{
+    CommunicationOverheadType, CommunicationType, EnergyConsumptionType, EnergyType,
+    ExchangeCostType, ExchangeType, MetricsType, StateCostType, InvolvedDevicesCount,
+};
+
+lazy_static! {
+    static ref EPSB: f32 = env::var("EPSB")
+        .unwrap_or(0.0001.to_string())
+        .parse::<f32>()
+        .unwrap();
+    static ref EPRB: f32 = env::var("EPRB")
+        .unwrap_or(0.0001.to_string())
+        .parse::<f32>()
+        .unwrap();
+    static ref SENT_MESSAGE_SIZE: u32 = env::var("SENT_MESSAGE_SIZE")
+        .unwrap_or(16.to_string())
+        .parse::<u32>()
+        .unwrap();
+    static ref RECEIVED_MESSAGE_SIZE: u32 = env::var("RECEIVED_MESSAGE_SIZE")
+        .unwrap_or(16.to_string())
+        .parse::<u32>()
+        .unwrap();
+}
+
+pub fn get_metrics(
+    number_of_nodes: u32,
+    number_of_gateway_members: u32,
+    number_of_neighbors: u32,
+) -> MetricsType {
+    let metrics = MetricsType {
+        energy: EnergyType {
+            compromised: EnergyConsumptionType {
+                constrained: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: 1,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *EPSB,
+                        received: *EPRB,
+                    },
+                    number_of_involved_devices: number_of_nodes,
+                    involved_devices: InvolvedDevicesCount::All,
+                },
+                gateway: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: number_of_nodes,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: 0f32,
+                        received: 0f32,
+                    },
+                    number_of_involved_devices: 1,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+                left: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 0,
+                        received: 0,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *EPSB,
+                        received: *EPRB,
+                    },
+                    number_of_involved_devices: 0,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+            },
+            draining: EnergyConsumptionType {
+                constrained: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: number_of_nodes - 1,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *EPSB,
+                        received: *EPRB,
+                    },
+                    number_of_involved_devices: number_of_nodes,
+                    involved_devices: InvolvedDevicesCount::All,
+                },
+                gateway: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: number_of_nodes,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: 0f32,
+                        received: 0f32,
+                    },
+                    number_of_involved_devices: 1,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+                left: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 0,
+                        received: 0,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *EPSB,
+                        received: *EPRB,
+                    },
+                    number_of_involved_devices: 0,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+            },
+            leaving: EnergyConsumptionType {
+                constrained: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: 1,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *EPSB,
+                        received: *EPRB,
+                    },
+                    number_of_involved_devices: number_of_neighbors,
+                    involved_devices: InvolvedDevicesCount::Neighbors,
+                },
+                gateway: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 0,
+                        received: 0,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: 0f32,
+                        received: 0f32,
+                    },
+                    number_of_involved_devices: 0,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+                left: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: number_of_neighbors,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *EPSB,
+                        received: *EPRB,
+                    },
+                    number_of_involved_devices: 1,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+            },
+        },
+        communication: CommunicationType {
+            compromised: CommunicationOverheadType {
+                constrained: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: 1,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: number_of_nodes,
+                    involved_devices: InvolvedDevicesCount::All,
+                },
+                gateway: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: number_of_nodes,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: 1,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+                left: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 0,
+                        received: 0,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: 0,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+            },
+            draining: CommunicationOverheadType {
+                constrained: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: number_of_nodes - 1,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: number_of_nodes,
+                    involved_devices: InvolvedDevicesCount::All,
+                },
+                gateway: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: number_of_nodes - 1,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: 1,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+                left: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 0,
+                        received: 0,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: 0,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+            },
+            leaving: CommunicationOverheadType {
+                constrained: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: 1,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: number_of_neighbors,
+                    involved_devices: InvolvedDevicesCount::Neighbors,
+                },
+                gateway: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 0,
+                        received: 0,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: 1,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+                left: StateCostType {
+                    exchange: ExchangeType {
+                        sent: 1,
+                        received: number_of_neighbors,
+                    },
+                    exchange_cost: ExchangeCostType {
+                        sent: *SENT_MESSAGE_SIZE as f32,
+                        received: *RECEIVED_MESSAGE_SIZE as f32,
+                    },
+                    number_of_involved_devices: 1,
+                    involved_devices: InvolvedDevicesCount::SameAsDefined,
+                },
+            },
+        },
+    };
+    metrics
+}
